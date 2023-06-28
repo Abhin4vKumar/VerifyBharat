6d1524020c1225d6972087ef81ce37df5a8efe5b
VerifyBharat4 <137670538+VerifyBharat4@users.noreply.github.com>
2023-06-28 23:33:12 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./middleware/error");
+var cors = require('cors') 
 
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 app.use(cookieParser());
