06acc4fe4aefa990686e39bd338a3aee17ff4a2d
VerifyBharat4 <137670538+VerifyBharat4@users.noreply.github.com>
2023-06-29 18:35:03 +0530

    Update jwtToken.js

--- Server/utils/jwtToken.js
+++ Server/utils/jwtToken.js
@@ -1,5 +1,6 @@
 const sendToken = (user, statusCode, res) => {
     const token = user.getJWTToken();
+    console.log("im at JWT " , token);
     // options for cookie
     const options = {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
