152564069fcc8ac48b168fd1cfeb3f2b15e35d2a
Abhin4vKumar <68866591+Abhin4vKumar@users.noreply.github.com>
2023-09-05 15:34:29 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 //Route Imports
-const certificate = require("./routes/certificateRoute");
-const user = require("./routes/userRoute");
-const organisation = require("./routes/organisationRoute");
+const certificate = require("./routes/certificatesRoutes");
+const user = require("./routes/usersRoutes");
+const organisation = require("./routes/organisationsRoutes");
 app.use("/api/v1", certificate);
 app.use("/api/v1", user);
 app.use("/api/v1", organisation);
